3e1f692f545984cafde73104c5b063ccb332dff8
Daniel Maizel <dani98000k@gmail.com>
2021-09-16 11:59:56 +0300

    Implemented the funcitonality of getting a value from a remote node

--- cache/http.go
+++ cache/http.go
@@ -2,19 +2,54 @@ package cache
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
+	"sync"
+
+	"github.com/dmaizel/distributed-cache/cache/consistenthash"
 )
 
-const defaultBasePath = "/_distcache/"
+const (
+	defaultBasePath = "/_distcache/"
+	defaultReplicas = 50
+)
 
 // HTTPPool implements PeerPicker for a pool of HTTP peers.
 // This is the core data structure for HTTP communication between nodes.
 type HTTPPool struct {
 	// this peer's base URL, e.g. "https://example.net:8000"
-	self     string
-	basePath string
+	self        string
+	basePath    string
+	sync.Mutex                         // guards peers and httpGetters
+	peers       *consistenthash.Map    // used to select nodes based on specific keys.
+	httpGetters map[string]*httpGetter // map the remote node to the corresponding httpGetter
+}
+
+type httpGetter struct {
+	baseURL string // represents the address of the remote node to be accessed
+}
+
+func (h *httpGetter) Get(group string, key string) ([]byte, error) {
+	u := fmt.Sprintf("%v%v/%v", h.baseURL, url.QueryEscape(group), url.QueryEscape(key))
+	res, err := http.Get(u)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("server returned %v", res.Status)
+	}
+
+	bytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("reading response body: %v", err)
+	}
+
+	return bytes, nil
 }
 
 // NewHTTPPool initializes an HTTP pool of peers.
@@ -61,3 +96,32 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/octet-stream")
 	w.Write(view.ByteSlice())
 }
+
+// Set updates the pool's list of peers.
+func (p *HTTPPool) Set(peers ...string) {
+	p.Lock()
+	defer p.Unlock()
+	p.peers = consistenthash.New(defaultReplicas, nil)
+	p.peers.Add(peers...)
+	p.httpGetters = make(map[string]*httpGetter, len(peers))
+
+	for _, peer := range peers {
+		p.httpGetters[peer] = &httpGetter{baseURL: peer + p.basePath}
+	}
+}
+
+// PickPeer picks a peer according to key
+func (p *HTTPPool) PickPeer(key string) (PeerGetter, bool) {
+	p.Lock()
+	defer p.Unlock()
+
+	if peer := p.peers.Get(key); peer != "" && peer != p.self {
+		p.Log("Pick peer %s", peer)
+		return p.httpGetters[peer], true
+	}
+
+	return nil, false
+}
+
+var _ PeerGetter = (*httpGetter)(nil)
+var _ PeerPicker = (*HTTPPool)(nil)
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -14,8 +15,52 @@ var db = map[string]string{
 	"Sam":  "567",
 }
 
+//func main() {
+//cache.NewGroup("scores", 2<<10, cache.GetterFunc(
+//func(key string) ([]byte, error) {
+//log.Println("[SlowDB] search key", key)
+//if v, ok := db[key]; ok {
+//return []byte(v), nil
+//}
+//return nil, fmt.Errorf("%s does not exist", key)
+//}))
+
+//addr := "localhost:9999"
+//peers := cache.NewHTTPPool(addr)
+//log.Println("distache is running at", addr)
+//log.Fatal(http.ListenAndServe(addr, peers))
+//}
+
 func main() {
-	cache.NewGroup("scores", 2<<10, cache.GetterFunc(
+	var port int
+	var api bool
+
+	flag.IntVar(&port, "port", 8001, "Distcache server port")
+	flag.BoolVar(&api, "api", false, "Start an api server?")
+	flag.Parse()
+
+	apiAddr := "http://localhost:9999"
+	addrMap := map[int]string{
+		8001: "http://localhost:8001",
+		8002: "http://localhost:8002",
+		8003: "http://localhost:8003",
+	}
+
+	var addrs []string
+	for _, v := range addrMap {
+		addrs = append(addrs, v)
+	}
+
+	group := createGroup()
+	if api {
+		go startAPIServer(apiAddr, group)
+	}
+
+	startCacheServer(addrMap[port], []string(addrs), group)
+}
+
+func createGroup() *cache.Group {
+	return cache.NewGroup("scores", 2<<10, cache.GetterFunc(
 		func(key string) ([]byte, error) {
 			log.Println("[SlowDB] search key", key)
 			if v, ok := db[key]; ok {
@@ -23,9 +68,29 @@ func main() {
 			}
 			return nil, fmt.Errorf("%s does not exist", key)
 		}))
+}
 
-	addr := "localhost:9999"
+func startAPIServer(apiAddr string, group *cache.Group) {
+	http.Handle("/api", http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			key := r.URL.Query().Get("key")
+			view, err := group.Get(key)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/octet-stream")
+			w.Write(view.ByteSlice())
+		}))
+
+	log.Println("fontend server is running at", apiAddr)
+	log.Fatal(http.ListenAndServe(apiAddr[7:], nil))
+}
+
+func startCacheServer(addr string, addrs []string, group *cache.Group) {
 	peers := cache.NewHTTPPool(addr)
-	log.Println("distache is running at", addr)
-	log.Fatal(http.ListenAndServe(addr, peers))
+	peers.Set(addrs...)
+	group.RegisterPeers(peers)
+	log.Println("distcache is running at", addr)
+	log.Fatal(http.ListenAndServe(addr[7:], peers))
 }
