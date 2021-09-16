0a77c801d21a95a7877464ceebdfede74a7ced1a
Daniel Maizel <dani98000k@gmail.com>
2021-09-16 07:21:03 +0300

    Added consistent hashing

---
+++ cache/consistenthash/consistenthash.go
@@ -0,0 +1,66 @@
+package consistenthash
+
+import (
+	"fmt"
+	"hash/crc32"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	fmt.Println("vim-go")
+}
+
+// Hash maps bytes to uint32
+type Hash func(data []byte) uint32
+
+// Map contains all hashed keys
+type Map struct {
+	hash     Hash
+	replicas int
+	keys     []int // Sorted
+	hashMap  map[int]string
+}
+
+// New creates a Map instance
+func New(replicas int, fn Hash) *Map {
+	m := &Map{
+		replicas: replicas,
+		hash:     fn,
+		hashMap:  make(map[int]string),
+	}
+
+	if m.hash == nil {
+		m.hash = crc32.ChecksumIEEE
+	}
+
+	return m
+}
+
+// Add adds some keys to the hash
+func (m *Map) Add(keys ...string) {
+	for _, key := range keys {
+		for i := 0; i < m.replicas; i++ {
+			hash := int(m.hash([]byte(strconv.Itoa(i) + key)))
+			m.keys = append(m.keys, hash)
+			m.hashMap[hash] = key
+		}
+	}
+	sort.Ints(m.keys)
+}
+
+// Get gets the closest item in the hash to the provided key.
+func (m *Map) Get(key string) string {
+	if len(m.keys) == 0 {
+		return ""
+	}
+
+	hash := int(m.hash([]byte(key)))
+	// Binary search for appropriate replica (find virtual node)
+	idx := sort.Search(len(m.keys), func(i int) bool {
+		return m.keys[i] >= hash
+	})
+
+	// Return the corresponding real node
+	return m.hashMap[m.keys[idx%len(m.keys)]]
+}
