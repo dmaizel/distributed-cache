40614eb6314e59dc7bce719c5b91634491f5d26c
Daniel Maizel <dani98000k@gmail.com>
2021-09-24 18:03:14 +0300

    Added protobuf support for better performance of communication between nodes.

--- cache/distcache.go
+++ cache/distcache.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"sync"
 
+	pb "github.com/dmaizel/distributed-cache/cache/cachepb"
 	"github.com/dmaizel/distributed-cache/cache/singleflight"
 )
 
@@ -123,10 +124,16 @@ func (g *Group) RegisterPeers(peers PeerPicker) {
 }
 
 func (g *Group) getFromPeer(peer PeerGetter, key string) (ByteView, error) {
-	bytes, err := peer.Get(g.name, key)
+	req := &pb.Request{
+		Group: g.name,
+		Key:   key,
+	}
+
+	res := &pb.Response{}
+	err := peer.Get(req, res)
 	if err != nil {
 		return ByteView{}, err
 	}
 
-	return ByteView{b: bytes}, nil
+	return ByteView{b: res.Value}, nil
 }
